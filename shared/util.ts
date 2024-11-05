62256f50136586d6ce14cba678e219dbf9bc3a2f
Dylan <dylanebp@hotmail.com>
2024-11-05 20:15:03 +0000

    Added auth api utility code

--- shared/util.ts
+++ shared/util.ts
@@ -1,6 +1,10 @@
 import { marshall } from "@aws-sdk/util-dynamodb";
 import { RetroGame } from "./types";
 import { TranslateClient, TranslateTextCommand } from "@aws-sdk/client-translate";
+import { APIGatewayRequestAuthorizerEvent, APIGatewayAuthorizerEvent, PolicyDocument, APIGatewayProxyEvent, StatementEffect } from "aws-lambda";
+import axios from "axios"
+import jwt from 'jsonwebtoken'
+import jwkToPem from "jwk-to-pem";
 
 const translateClient = new TranslateClient({ region: "eu-west-1" })
 
@@ -28,7 +32,7 @@ export async function translateJsonCollection(data: any, targetLanguageCode: str
       continue;
     }
 
-    if (typeof data[attribute] === "string"){
+    if (typeof data[attribute] === "string") {
       const command = new TranslateTextCommand({
         Text: data[attribute],
         SourceLanguageCode: "en",
@@ -44,3 +48,72 @@ export async function translateJsonCollection(data: any, targetLanguageCode: str
   translatedAttributes["lang"] = targetLanguageCode
   return translatedAttributes
 }
+
+
+// ----- Auth Code -----
+
+export type CookieMap = { [key: string]: string } | undefined;
+export type JwtToken = { sub: string; email: string } | null;
+export type Jwk = {
+  keys: {
+    alg: string;
+    e: string;
+    kid: string;
+    kty: string;
+    n: string;
+    use: string;
+  }[];
+};
+
+export const parseCookies = (
+  event: APIGatewayRequestAuthorizerEvent | APIGatewayProxyEvent
+) => {
+  if (!event.headers || !event.headers.Cookie) {
+    return undefined;
+  }
+
+  const cookiesStr = event.headers.Cookie;
+  const cookiesArr = cookiesStr.split(";");
+
+  const cookieMap: CookieMap = {};
+
+  for (let cookie of cookiesArr) {
+    const cookieSplit = cookie.trim().split("=");
+    cookieMap[cookieSplit[0]] = cookieSplit[1];
+  }
+
+  return cookieMap;
+};
+
+export const verifyToken = async (
+  token: string,
+  userPoolId: string | undefined,
+  region: string
+): Promise<JwtToken> => {
+  try {
+    const url = `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
+    const { data }: { data: Jwk } = await axios.get(url);
+    const pem = jwkToPem(data.keys[0]);
+
+    return jwt.verify(token, pem, { algorithms: ["RS256"] });
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+export const createPolicy = (
+  event: APIGatewayAuthorizerEvent,
+  effect: StatementEffect
+): PolicyDocument => {
+  return {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: effect,
+        Action: "execute-api:Invoke",
+        Resource: [event.methodArn],
+      },
+    ],
+  };
+};
